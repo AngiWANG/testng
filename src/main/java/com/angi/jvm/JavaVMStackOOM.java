5090129b02785cd96c386261c27c5c5a1238d33c
Angi WANG <angi.wang.afe@gmail.com>
2014-09-03 15:02:41 +0800

    java vm stack oom. runtime constant pool oom.

---
+++ src/main/java/com/angi/jvm/JavaVMStackOOM.java
@@ -0,0 +1,32 @@
+package com.angi.jvm;
+
+/**
+ * VM Args:-Xss2m
+ * 
+ * @author Angi
+ * 
+ */
+public class JavaVMStackOOM {
+
+	private void dontStop() {
+		while (true) {
+
+		}
+	}
+
+	public void stackLeakByThread() {
+		while (true) {
+			Thread thread = new Thread(new Runnable() {
+				public void run() {
+					dontStop();
+				}
+			});
+			thread.start();
+		}
+	}
+
+	public static void main(String[] args) throws Throwable {
+		JavaVMStackOOM oom = new JavaVMStackOOM();
+		oom.stackLeakByThread();
+	}
+}
