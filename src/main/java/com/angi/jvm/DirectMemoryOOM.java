0618c7ca9ad16be34e1199cd373d728b6149e3b2
Angi WANG <angi.wang.afe@gmail.com>
2014-09-03 15:26:50 +0800

    direct memory oom

---
+++ src/main/java/com/angi/jvm/DirectMemoryOOM.java
@@ -0,0 +1,25 @@
+package com.angi.jvm;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+/**
+ * VM Args:-Xmx20M -XX:DirectMemorySize=10M
+ * 
+ * @author Angi
+ * 
+ */
+public class DirectMemoryOOM {
+
+	private static final int _1MB = 1024 * 1024;
+
+	public static void main(String[] args) throws Throwable {
+		Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+		unsafeField.setAccessible(true);
+		Unsafe unsafe = (Unsafe) unsafeField.get(null);
+		while (true) {
+			unsafe.allocateMemory(_1MB);
+		}
+	}
+}
