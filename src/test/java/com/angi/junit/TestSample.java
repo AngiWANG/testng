0b501ccd9037c6e29b1cc569feaf8965dc54a6dd
Angi WANG <angi.wang.afe@gmail.com>
2015-01-05 17:26:06 +0800

    initial.

---
+++ src/test/java/com/angi/junit/TestSample.java
@@ -0,0 +1,13 @@
+package com.angi.junit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestSample {
+	@Test
+	public void add() {
+		assertTrue(true);
+	}
+
+}
